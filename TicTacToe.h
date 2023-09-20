41c646112d1aa5b68a72462d00d52fac74cf5770
Erik Santana <eriksantana714@outlook.com>
2023-09-20 16:07:47 -0700

    updated pre and post

--- TicTacToe.h
+++ TicTacToe.h
@@ -20,18 +20,60 @@ private:
 	int gamesPlayed;
 
 public:
+
+	// Pre-Condition: no parameters
+	// Post-Condition: no return. Initializer Constructor.
 	TicTacToe();
+
+	// Pre-Condition: no parameters
+	// Post-Condition: returns row. Gets the rows
 	int getRow() const;
+
+	// Pre-Condition: parameters: int newRow
+	// Post-Condition: no return. Sets the rows
 	void setRow(int newRow);
+
+	// Pre-Condition: no parameters.
+	// Post-Condition: no return. Gets the columns.
 	int getColumn() const;
+
+	// Pre-Condition: parameters: int newColumn
+	// Post-Condition: no return. Sets the Column
 	void setColumn(int newColumn);
+
+	// Pre-Condition: no parameters
+	// Post-Condition: no return. This is the main function of the game engine tic-tac-toe
 	void ticTacToe();
+
+	// Pre-Condition: parameters: char playerSymbol &  int& moves used for the current player and keeping track of # of moves
+	// Post-Condition: no return. This is for the human to input their move.
 	void makeMove(char playerSymbol, int& moves);
+
+	// Pre-Condition: no parameters
+	// Post-Condition: no return. Updates the board
 	void updateBoard();
+
+	// Pre-Condition: parameters: char playerSymbol
+	// Post-Condition: returns a boolean. Checks for winner
 	bool checkWin(char playerSymbol);
+
+	// Pre-Condition: no parameters
+	// Post-Condition: returns a boolean. Checks for draw
 	bool checkDraw();
+
+	// Pre-Condition: no parameters
+	// Post-Condition: no return. Initializes the random number generator with a seed.
 	void initRandom();
+
+	// Pre-Condition: parameters: char playerSymbol. Takes in the computer move and randomizes the position.
+	// Post-Condition: no return
 	void computerMove(char playerSymbol);
+
+	// Pre-Condition: no parameters
+	// Post-Condition: no return. Resets the board to empty spaces.
 	void resetBoard();
+
+	// Pre-Condition: parameters: double fastTime, int fastTimeMoves, double longTime, int longTimeMoves, double sum
+	// Post-Condition: no return. Provides game statistics.
 	void gameStatistics(double fastTime, int fastTimeMoves, double longTime, int longTimeMoves, double sum);
 };
